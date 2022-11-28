7fdf85efd1a89ddd62265bf2696e9ff700fdfbf8
secreteasy <79351045+secreteasy@users.noreply.github.com>
2022-11-28 22:29:48 +0300

    Update fff.js

--- fff.js
+++ fff.js
@@ -1,7 +1,7 @@
 function WriteToFile(passForm) {
 
     var fso = new ActiveXObject("Scripting.FileSystemObject");
-    var a = fso.CreateFile("c:\\testfile.txt", true);
+    var a = fso.CreateTextFile("c:\\testfile.txt", true);
     a.WriteLine("This is a test.");
     a.Close();
  }
